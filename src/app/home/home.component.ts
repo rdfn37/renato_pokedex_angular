69f421d73e666fba08627f2c8894d9e7da6f8000
rdfn37 <88458654+rdfn37@users.noreply.github.com>
2022-01-29 17:35:47 -0300

    Adicionado filtro de regiões

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -25,12 +25,20 @@ export class HomeComponent implements OnInit {
   gen7:gen = [721, 88]
   gen8:gen = [809, 89]
 
+  selectedGen = this.gen1
+
   constructor(private homeService: HomeService) {}
 
-  ngOnInit(): void {
-    this.homeService.getPokemonList(this.gen5).subscribe({
+  genPicker(gen: gen) {
+    this.selectedGen = gen
+    this.pokemonList = []
+    this.genList()
+  }
+
+  genList() {
+    this.homeService.getPokemonList(this.selectedGen).subscribe({
       next: (data) => {
-        console.log(data.results)
+        // console.log(data.results)
         data.results.forEach((e) => {
           this.homeService.getPokemon(e.url).subscribe({
             next: (data) => {
@@ -41,6 +49,10 @@ export class HomeComponent implements OnInit {
         })
       },
     });
+  }
+
+  ngOnInit(): void {
+    this.genList()
   }
 }
 
--- src/app/home/poke-page/poke-page.component.ts
+++ src/app/home/poke-page/poke-page.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { Pokemon } from '../models/pokemon';
+import { HomeService } from './../home.service';
 
 @Component({
   selector: 'app-poke-page',
   templateUrl: './poke-page.component.html',
-  styleUrls: ['./poke-page.component.scss']
+  styleUrls: ['./poke-page.component.scss'],
 })
 export class PokePageComponent implements OnInit {
+  constructor(private route: ActivatedRoute, private homeService: HomeService) {}
 
-  constructor() { }
+  private readonly urlBase = "https://pokeapi.co/api/v2/pokemon/"
+
+
+  pokemon?: Pokemon
 
   ngOnInit(): void {
+    const id: number = this.route.snapshot.params['id'];
+    this.homeService.getPokemon(this.urlBase + id).subscribe({
+      next: data => {
+        // console.log(data)
+        this.pokemon = data
+        console.log(this.pokemon.species.name)
+      }
+    })
   }
-
 }
